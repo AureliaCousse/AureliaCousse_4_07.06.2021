7f22711dc2cbc8e10bf942164cd643e80a3a779f
AureliaCousse <80416325+AureliaCousse@users.noreply.github.com>
2021-06-29 17:40:50 +0200

    Update 2021/06/29 17h40

--- modal.js
+++ modal.js
@@ -36,27 +36,32 @@ function closeModal(){
 //#2 Validation des input saisis.
 
 let myForm = document.getElementById('myForm');
-let firstRegex = /^[a-zA-Z-\s]+$/; //accepte minuscule majuscule tiret et espaces
+
+
+myForm.addEventListener('submit', function(e) {
+e.preventDefault(); // si vide ou false, entrée sera refusée au moment du submit A mettre juste après la fonction pour éviter de répéter 
+
+
+let firstRegex = /^[a-zA-Z-\s]+$/; //accepte minuscules majuscules tirets et espaces only
 let lastRegex = /^[a-zA-Z-\s]+$/;
 let mailRegex = /^[a-zA-Z][a-zA-Z\d-\.]*@[a-zA-Z]+\.[a-zA-Z]{2,3}$/; //\d = [0-9] \. = caractere "." += 1 ou plus / *= 0 ou plus
 let gamesRegex = /^[0-99]$/;
-let birthdateRegex = /^[1-31]\/[1-12]\/[1900-2050]$/;
 
-myForm.addEventListener('submit', function(e) {
- 
+var birthDate = document.querySelector('input[type="birthdate"]');
+birthDate.value = '1900-00-01';
+
   /*check prénom*/
   let userFirst = document.getElementById("first"); //on récupère l'input que l'on veut checker
 
   if (userFirst.value.trim().length <2){   //fonction trim: pour supprimer les espaces de début et de fin
     let firstError = document.getElementById('firstError');
-    firstError.innerHTML = "Veuillez saisir un prénom à 2 lettres minimum.";
+    firstError.innerHTML = "Veuillez saisir votre prénom (2 lettres minimum).";
     firstError.style.color = "red";
-    e.preventDefault(); // si "" cad vide, entrée sera refusée au moment du submit 
+     
   } else if (firstRegex.test(userFirst.value) == false) {
-    let firstError = document.getElementById('error');
+    let firstError = document.getElementById('firstError');
     firstError.innerHTML = "Le nom doit comporter des lettres, tiret uniquement.";
-    firstError.style.color = "red";
-    e.preventDefault(); // si "" cad vide, entrée sera refusée au moment du submit 
+    firstError.style.color = "red"; 
   } else {firstError.innerHTML = "";
   }
 
@@ -65,30 +70,26 @@ myForm.addEventListener('submit', function(e) {
 
   if (userLast.value.trim().length <2){   
     let firstError = document.getElementById('lastError');
-    lastError.innerHTML = "Veuillez saisir un nom à 2 lettres minimum.";
+    lastError.innerHTML = "Veuillez saisir votre nom (2 lettres minimum).";
     lastError.style.color = "red";
-    e.preventDefault();  
   } else if (lastRegex.test(userLast.value) == false) {
-    let lastError = document.getElementById('error');
+    let lastError = document.getElementById('lastError');
     lastError.innerHTML = "Le nom doit comporter des lettres, tiret uniquement.";
     lastError.style.color = "red";
-    e.preventDefault(); 
   }else {lastError.innerHTML = "";
   }
   
 /*check email*/
 let userMail = document.getElementById("email"); 
 
-if (userMail.value.trim()== ""){   
+if (userMail.value.trim() == ""){   
   let mailError = document.getElementById('mailError');
-  mailError.innerHTML = "Veuillez saisir une adresse mail.";
+  mailError.innerHTML = "Veuillez saisir votre adresse mail.";
   mailError.style.color = "red";
-  e.preventDefault(); 
 } else if (mailRegex.test(userMail.value) == false) {
   let mailError = document.getElementById('mailError');
   mailError.innerHTML = "Veuillez indiquer une adresse mail valide.";
   mailError.style.color = "red";
-  e.preventDefault(); 
 } else {mailError.innerHTML = "";
 }
 
@@ -96,32 +97,28 @@ if (userMail.value.trim()== ""){
 /*check quantity games  */
 let numberGames = document.getElementById("quantity"); 
 
-if (numberGames.value.trim()== ""){   
+if (numberGames.value.trim() == ""){   
   let gamesError = document.getElementById('gamesError');
-  gamesError.innerHTML = "Veuillez saisir un nombre de 0 à 99.";
+  gamesError.innerHTML = "Veuillez indiquer votre nombre de participations aux tournois GameOn.";
   gamesError.style.color = "red";
-  e.preventDefault();  
 } else if (gamesRegex.test(numberGames.value) == false) {
   let gamesError = document.getElementById('error');
-  gamesError.innerHTML = "Veuillez sélectionner un nombre de 0 à 99.";
+  gamesError.innerHTML = "Veuillez saisir ou sélectionner un nombre de 0 à 99.";
   gamesError.style.color = "red";
-  e.preventDefault(); 
 } else {gamesError.innerHTML = "";
 }
 
 /*check birthdate*/
 let birthdate= document.getElementById("birthdate"); 
 
-if (birthdate.value == ""){   
+if (birthdate.value === ""){   
   let birthdateError = document.getElementById('birthdateError');
   birthdateError.innerHTML = "Veuillez saisir une date valide sous le format JJ/MM/AAAA.";
   birthdateError.style.color = "red";
-  e.preventDefault();  
 } else if (birthdateRegex.test(birthdate.value) == false) {
   let birthDate = document.getElementById('birthdateError');
   birthdateErrorr.innerHTML = "Veuillez saisir ou sélectionner une date sous le format JJ/MM/AAAA.";
   birthdateErrorr.style.color = "red";
-  e.preventDefault(); 
 } else {birthdateError.innerHTML = "";
 }
 
