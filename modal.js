6624a3618c9509e37674c57994abf86a83696df3
AureliaCousse <80416325+AureliaCousse@users.noreply.github.com>
2021-07-05 18:56:04 +0200

    Update 2021/07/05 18h55

--- modal.js
+++ modal.js
@@ -50,8 +50,6 @@ e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au mo
   let lastRegex = /^[a-zA-Z-\s]+$/;
   let mailRegex = /^[a-zA-Z][a-zA-Z\d-\.]*@[a-zA-Z]+\.[a-zA-Z]{2,3}$/; //\d = [0-9] \. = caractere "." += 1 ou plus / *= 0 ou plus
   let gamesRegex = /^[0-99]$/;
-  // let birthdateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[1-9]|2[1-9]3[1-9]|4[1-9])$/;
-  // let birthdateRegex = /^[1-31]\/[1-12]\/[1900-2050]$/;
 
   // on récupère l'input que l'on veut checker:
   let userFirst = document.getElementById("first");
@@ -59,19 +57,29 @@ e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au mo
   let userMail = document.getElementById("email");
   let numberGames = document.getElementById("quantity"); 
   let birthDate = new Date(document.getElementById("birthdate").value); 
-  let cgv = document.getElementById("checkbox1"); 
+  let checkbox1 = document.getElementById("checkbox1"); 
   
+  let firstError = document.getElementById('firstError');
+  let lastError = document.getElementById('lastError');
+  let mailError = document.getElementById('mailError');
+  let gamesError = document.getElementById('gamesError');
+  let locationError = document.getElementById('locationError');
+  
+  
+
+  // let birthdateError = document.getElementById('birthdateError'); : 
+  // variable kept with birthdate block otherwise pb with form after error; even if data ok, still error message 
+
   console.log(birthDate)
+  console.log(checkbox1)
 
 //   /*check prénom*/
 
   if (userFirst.value.trim().length <2){   //fonction trim: pour supprimer les espaces de début et de fin
-    let firstError = document.getElementById('firstError');
     firstError.innerHTML = "Veuillez saisir un prénom à 2 lettres minimum.";
     firstError.style.color = "red";
     
   } else if (firstRegex.test(userFirst.value) == false) {
-    let firstError = document.getElementById('firstError');
     firstError.innerHTML = "Le nom doit comporter des lettres, tiret uniquement.";
     firstError.style.color = "red";
     
@@ -81,13 +89,11 @@ e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au mo
 
   /*check nom*/
 
-  if (userLast.value.trim().length <2){   
-    let lastError = document.getElementById('lastError');
+  if (userLast.value.trim().length <2){     
     lastError.innerHTML = "Veuillez saisir un nom à 2 lettres minimum.";
     lastError.style.color = "red";
     
   } else if (lastRegex.test(userLast.value) == false) {
-    let lastError = document.getElementById('lastError');
     lastError.innerHTML = "Le nom doit comporter des lettres, tiret uniquement.";
     lastError.style.color = "red";
     
@@ -97,27 +103,43 @@ e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au mo
 //   /*check email*/
 
   if (userMail.value.trim()== ""){   
-  let mailError = document.getElementById('mailError');
-  mailError.innerHTML = "Veuillez saisir une adresse mail.";
-  mailError.style.color = "red";
+    mailError.innerHTML = "Veuillez saisir une adresse mail.";
+    mailError.style.color = "red";
   
   } else if (mailRegex.test(userMail.value) == false) {
-  let mailError = document.getElementById('mailError');
-  mailError.innerHTML = "Veuillez indiquer une adresse mail valide.";
-  mailError.style.color = "red";
+    mailError.innerHTML = "Veuillez indiquer une adresse mail valide.";
+    mailError.style.color = "red";
 
   } else {mailError.innerHTML = "";
   }
 
+
+  /*check birthdate*/ 
+
+  if (isNaN(birthDate.getTime())) { // NaN: Not a Number =>date is not valid
+    let birthdateError = document.getElementById('birthdateError');
+    birthdateError.innerHTML = "Veuillez saisir une date valide.";
+    birthdateError.style.color = "red";
+    } else {
+      const now=new Date();
+    if (now.getTime()-birthDate.getTime()<18*31556952000){
+    let birthdateError = document.getElementById('birthdateError');
+    birthdateError.innerHTML = "Vous devez avoir plus de 18 ans.";
+    birthdateError.style.color = "red";
+    
+    } else {
+      let birthDateError = document.getElementById('birthdateError');
+      birthDateError.innerHTML = "";
+  }
+}
+
   /*check quantity games*/
 
-  if (numberGames.value.trim()== ""){   
-    let gamesError = document.getElementById('gamesError');
+  if (numberGames.value.trim()== ""){     
     gamesError.innerHTML = "Veuillez saisir un nombre de 0 à 99.";
     gamesError.style.color = "red";
     
     } else if (gamesRegex.test(numberGames.value) == false) {
-    let gamesError = document.getElementById('gamesError');
     gamesError.innerHTML = "Veuillez sélectionner un nombre de 0 à 99.";
     gamesError.style.color = "red";
   
@@ -125,48 +147,24 @@ e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au mo
     }
 
 
-/*check birthdate*/ 
-
-    if (isNaN(birthDate.getTime())) { // NaN: Not a Number =>date is not valid
-      let birthdateError = document.getElementById('birthdateError');
-      birthdateError.innerHTML = "Veuillez saisir une date valide.";
-      birthdateError.style.color = "red";
-      } else {
-        const now=new Date();
-      if (now.getTime()-birthDate.getTime()<18*31556952000){
-      let birthdateError = document.getElementById('birthdateError');
-      birthdateError.innerHTML = "Vous devez avoir plus de 18 ans.";
-      birthdateError.style.color = "red";
-      
-      } else {
-        let birthDateError = document.getElementById('birthdateError');
-        birthDateError.innerHTML = "";
-      
-    }
-  }
-
   /*check location*/ 
 
   if (!check()){
-    let locationError = document.getElementById('locationError');
-    locationError.innerHTML = "Vous devez choisir une location.";
+    locationError.innerHTML = "Vous devez sélectionner une ville.";
     locationError.style.color = "red";
     
   } else {
-    let locationError = document.getElementById('locationError');
     locationError.innerHTML = "";
   }
 
 /*check Acceptation CGU*/
 
-    if (!cgu.checked) {   /*attend true ou false donc pas besoin de mettre ==*/
-      let cguError = document.getElementById('cguError');
-      cguError.innerHTML = "Vous devez confirmer avoir lu et accepté les condition d'utilisation.";
-      cguError.style.color = "red";
+    if (!checkbox1.checked) {   /*attend true ou false donc pas besoin de mettre ==*/
+      checkbox1Error.innerHTML = "Vous devez lire et accepter les conditions d'utilisation.";
+      checkbox1Error.style.color = "red";
       
     } else {
-      let cguError = document.getElementById('cguError');
-      cguError.innerHTML = "";
+      checkbox1Error.innerHTML = "";
     }
     
 } 
@@ -201,4 +199,3 @@ function check(){
 //         return confirm('Merci! votre réservation a bien été prise en compte.');
 // }
 // }
-
