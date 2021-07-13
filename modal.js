eab71816dcf9fba2a4ffb0bc16691fee450bd237
AureliaCousse <80416325+AureliaCousse@users.noreply.github.com>
2021-07-13 18:13:50 +0200

    Update 2021/07/13 18h15

--- modal.js
+++ modal.js
@@ -19,51 +19,38 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
 //////////////////////////////////////////////////////////////////////////////
 
 /* close modal form*/
-
-function closeModal(){
-  modalbg.style.display = 'none'; 
-  //modalbg: name previously given to the form container (see DOM elements)
+function closeModal() {
+  modalbg.style.display = 'none'; //modalbg: name previously given to the form container (see DOM elements)
   myForm.style.display = "block";
   document.getElementById("confirmation").style.display = "none";
 }
 
-
-
 const closeBtn = document.querySelectorAll(".close");
-  closeBtn.forEach((btn) => btn.addEventListener('click', closeModal));
-  //closeBtn, btn, closeModal: new names given to elements to create function
-
-
+closeBtn.forEach((btn) => btn.addEventListener('click', closeModal)); //closeBtn, btn, closeModal: new names given to elements to create function
 
 /*Function check radio buttons*/
-
-function check(){
+function check() {
   let radio = document.getElementsByName('location');
   let result = false;
 
   for (let i = 0, len = radio.length; i < len; i++) {
     if (radio[i].checked) {
-      result = true;  
+      result = true;
     }
   }
   return result
-  }
-
+}
 
 /*Check & validate user's inputs*/
-
 let myForm = document.getElementById('myForm');
-myForm.addEventListener('submit', function(e) { 
+myForm.addEventListener('submit', function (e) {
 
-e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au moment du submit 
+  e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au moment du submit 
 
-// To confirm what is valid in inputs fields:
-
+  // To confirm what is valid in inputs fields:
   let firstRegex = /^[a-zA-Z-\s]+$/; //accepte minuscule majuscule tiret et espaces
   let lastRegex = /^[a-zA-Z-\s]+$/;
   let mailRegex = /^[a-zA-Z][a-zA-Z\d-\.]*@[a-zA-Z]+\.[a-zA-Z]{2,3}$/; //   (\d = [0-9]) (\. = caractere ".") (+= 1 ou plus) ( *= 0 ou plus)
@@ -73,11 +60,10 @@ e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au mo
   let userFirst = document.getElementById("first");
   let userLast = document.getElementById("last");
   let userMail = document.getElementById("email");
-  let birthDate = new Date(document.getElementById("birthdate").value); 
-  let numberGames = document.getElementById("quantity"); 
-  let checkbox1 = document.getElementById("checkbox1"); 
-  
-  
+  let birthDate = new Date(document.getElementById("birthdate").value);
+  let numberGames = document.getElementById("quantity");
+  let checkbox1 = document.getElementById("checkbox1");
+
   // To find out errors:
   let firstError = document.getElementById('firstError');
   let lastError = document.getElementById('lastError');
@@ -85,36 +71,28 @@ e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au mo
   let birthdateError = document.getElementById('birthdateError');
   let gamesError = document.getElementById('gamesError');
   let locationError = document.getElementById('locationError');
-  
-  
+
   // To consider that inputs are true by default
   let isSubmitValid = true;
 
-
-  // lastError.innerHTML = ""; idem pour le reste
-  // gamesError.innerHTML = "";
+  //....unless error is found => false
   locationError.innerHTML = "";
-  // checkbox1Error.innerHTML = "";
-  // birthDateError.innerHTML = "";
-
 
   /*check prénom*/
-  
-  if (userFirst.value.trim().length <2){   //fonction trim: pour supprimer les espaces de début et de fin
-        isSubmitValid = false;
-        showError(userFirst, firstError, "Veuillez saisir un prénom à 2 lettres minimum.");
+  if (userFirst.value.trim().length < 2) {   //fonction trim: pour supprimer les espaces de début et de fin
+    isSubmitValid = false;
+    showError(userFirst, firstError, "Veuillez saisir un prénom à 2 lettres minimum.");
   }
   else if (firstRegex.test(userFirst.value) == false) {
     showError(userFirst, firstError, "Le prénom doit comporter des lettres, tiret uniquement.");
     isSubmitValid = false;
-  }  
-  else{
+  }
+  else {
     removeError(userFirst, firstError)
   }
-  
-  /*check nom*/
 
-  if (userLast.value.trim().length <2){     
+  /*check nom*/
+  if (userLast.value.trim().length < 2) {
     showError(userLast, lastError, "Veuillez saisir un nom à 2 lettres minimum.");
     isSubmitValid = false;
   }
@@ -122,99 +100,94 @@ e.preventDefault(); // si "" cad vide ou si error, l'entrée sera refusée au mo
     showError(userLast, lastError, "Le nom doit comporter des lettres, tiret uniquement.");
     isSubmitValid = false;
   }
-  else{
+  else {
     removeError(userLast, lastError)
   }
 
   /*check email*/
-
-  if (userMail.value.trim()== ""){  
+  if (userMail.value.trim() == "") {
     showError(userMail, mailError, "Veuillez saisir une adresse mail.");
     isSubmitValid = false;
-  } 
+  }
   else if (mailRegex.test(userMail.value.trim()) == false) {
     showError(userMail, mailError, "Veuillez indiquer une adresse mail valide.");
     isSubmitValid = false;
-  } 
-  else{
+  }
+  else {
     removeError(userMail, mailError)
   }
 
-  /*check birthdate*/ 
-
+  /*check birthdate*/
   if (isNaN(birthDate.getTime())) { // NaN: Not a Number =>date is not valid
     showError(birthdate, birthdateError, "Veuillez saisir une date valide.");
     isSubmitValid = false;
-  } 
-    
+  }
   else {   // check that user is over 18 years old
-      const now=new Date();
-    
-    if (now.getTime()-birthDate.getTime()<18*31556952000){
+    const now = new Date();
+
+    if (now.getTime() - birthDate.getTime() < 18 * 31556952000) {
       showError(birthdate, birthdateError, "Vous devez avoir plus de 18 ans.");
       isSubmitValid = false;
-    } 
-  else{
+    }
+    else {
       removeError(birthdate, birthdateError)
+    }
   }
-} 
 
   /*check quantity games*/
-
-  if (numberGames.value.trim()== ""){     
+  if (numberGames.value.trim() == "") {
     showError(numberGames, gamesError, "Veuillez saisir un nombre de 0 à 99.");
     isSubmitValid = false;
-    
+
   } else if (gamesRegex.test(numberGames.value) == false) {
     showError(numberGames, gamesError, "Veuillez sélectionner un nombre de 0 à 99.");
     isSubmitValid = false;
-  }  
-  else{
+  }
+  else {
     removeError(numberGames, gamesError)
   }
-    
-  /*check location*/ 
 
-  if (!check()){
+  /*check location*/
+  if (!check()) {
     locationError.innerHTML = "Vous devez sélectionner une ville.";
     locationError.style.color = "red";
     isSubmitValid = false;
-  } 
+  }
 
-/*check Acceptation CGU*/
+  /*check Acceptation CGU*/
+  if (!checkbox1.checked) {   /*attend true ou false donc pas besoin de mettre ==*/
+    showError(checkbox1, checkbox1Error, "Vous devez lire et accepter les conditions d'utilisation.");
+    isSubmitValid = false;
+  }
+  else {
+    removeError(checkbox1, checkbox1Error)
+  }
 
-    if (!checkbox1.checked) {   /*attend true ou false donc pas besoin de mettre ==*/
-      showError(checkbox1, checkbox1Error, "Vous devez lire et accepter les conditions d'utilisation.");
-      isSubmitValid = false;
-    }
+  /* To send form and get a confirmation message*/
+  if (isSubmitValid != false) {
+    myForm.style.display = "none";
+    document.getElementById("confirmation").style.display = "block";
+  }
 
-/* To send form with confirmation message*/
-    
-    if (isSubmitValid != false) {
-      myForm.style.display = "none";
-      document.getElementById("confirmation").style.display = "block";
-    }
-
-  } 
-)
+});
 
 /**
  * Delete Error message
  * @param {*} input 
  * @param {*} error 
  */
-function removeError(input, error){
+function removeError(input, error) {
   error.innerHTML = "";
   input.parentNode.removeAttribute("data-error-visible");
 }
 
 /**
  * Show Error message
- * @param {*} input 
- * @param {*} error 
- * @param {*} errorMessage 
+ * @param {*} input: champs rempli par le user
+ * @param {*} error: ligne contenant les message d'erreur si saisie du user erronée
+ * @param {*} errorMessage : message d'erreur lui-même
  */
-function showError(input, error, errorMessage){
+function showError(input, error, errorMessage) {
   error.innerHTML = errorMessage;
   error.style.color = "red";
   input.parentNode.setAttribute("data-error-visible", "true");
